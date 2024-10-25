73ba2865732d74d928793c68294fcc9c64b03e56
Dune Desormeaux <dune.desormeaux@gmail.com>
2024-10-25 11:06:33 -0700

    changes to support run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd /tmp/patch
-sudo ~/scripts/start-jack.sh
+~/scripts/start-jack.sh
 echo "starting supercollider from run.sh"
-/usr/local/bin/sclang ./run_me.scd & echo $! > /tmp/pids/sclang.pid
+/usr/local/bin/sclang ./mother.scd & echo $! > /tmp/pids/sclang.pid
