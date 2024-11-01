2e5cc1b1041ea5a2e807579b2d5493dcfc05cfad
Dune Desormeaux <dune.desormeaux@gmail.com>
2024-11-01 11:59:16 -0700

    explicit install order

--- deploy.sh
+++ deploy.sh
@@ -35,7 +35,11 @@ if [ ! $? -eq 0 ]; then
     oscsend localhost 4001 /oled/aux/line/2 s "jackd"
     sudo ~/scripts/remount-rw.sh
     echo 'jackd2 jackd/tweak_rt_limits boolean true' | sudo debconf-set-selections
-    sudo dpkg -i jackd/*.deb
+    sudo dpkg -i jackd/libjack-jackd2-0_1.9.12~dfsg-2_armhf.deb
+    sudo dpkg -i jackd/jackd_5_all.deb
+    sudo dpkg -i jackd/qjackctl_0.4.4-1_armhf.deb
+    sudo dpkg -i jackd/jackd2_1.9.12~dfsg-2_armhf.deb
+
     if [ ! $? -eq 0 ]; then
         oscsend localhost 4001 /oled/aux/line/1 s "jackd install failed"
         sudo ~/scripts/remount-ro.sh
