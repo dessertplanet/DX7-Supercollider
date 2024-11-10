2c0216bb529ce058e1319e02eb8cd10539c655f1
Dune Desormeaux <dune.desormeaux@gmail.com>
2024-11-10 17:14:09 -0500

    Restructure directories and update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,8 +20,8 @@ if ! [ -f /usr/local/bin/supernova ]; then
     oscsend localhost 4001 /oled/aux/line/2 s "Supercollider v3.11.2"
 
     sudo ~/scripts/remount-rw.sh
-    sudo unzip -o sc11.zip  -d /usr/local
-    /usr/local/bin/sclang ./firstrun.scd
+    sudo unzip -o prereqs/sc/sc11.zip  -d /usr/local
+    /usr/local/bin/sclang prereqs/sc/firstrun.scd
     sudo ~/scripts/remount-ro.sh
 fi
 
@@ -35,10 +35,10 @@ if [ ! $? -eq 0 ]; then
     oscsend localhost 4001 /oled/aux/line/2 s "jackd"
     sudo ~/scripts/remount-rw.sh
     echo 'jackd2 jackd/tweak_rt_limits boolean true' | sudo debconf-set-selections
-    sudo dpkg -i jackd/libjack-jackd2-0_1.9.12~dfsg-2_armhf.deb
-    sudo dpkg -i jackd/jackd2_1.9.12~dfsg-2_armhf.deb
-    sudo dpkg -i jackd/jackd_5_all.deb
-    sudo dpkg -i jackd/qjackctl_0.4.4-1_armhf.deb
+    sudo dpkg -i prereqs/jackd/libjack-jackd2-0_1.9.12~dfsg-2_armhf.deb
+    sudo dpkg -i prereqs/jackd/jackd2_1.9.12~dfsg-2_armhf.deb
+    sudo dpkg -i prereqs/jackd/jackd_5_all.deb
+    sudo dpkg -i prereqs/jackd/qjackctl_0.4.4-1_armhf.deb
 
     if [ ! $? -eq 0 ]; then
         oscsend localhost 4001 /oled/aux/line/1 s "jackd install failed"
@@ -51,11 +51,17 @@ if [ ! $? -eq 0 ]; then
 fi
 
 if ! [ -f /usr/local/share/SuperCollider/Extensions ]; then
+    oscsend localhost 4001 /oled/gClear ii 3 1
+    oscsend localhost 4001 /oled/aux/line/1 s "Installing prereq"
+    oscsend localhost 4001 /oled/aux/line/2 s "SC Extensions"
     sudo ~/scripts/remount-rw.sh
     sudo mkdir /usr/local/share/SuperCollider/Extensions
-    sudo cp ./FM7* /usr/local/share/SuperCollider/Extensions
     sudo ~/scripts/remount-ro.sh
 fi
+
+sudo ~/scripts/remount-rw.sh
+sudo cp -f prereqs/extensions/* /usr/local/share/SuperCollider/Extensions
+sudo ~/scripts/remount-ro.sh
 
 exit 1
 
